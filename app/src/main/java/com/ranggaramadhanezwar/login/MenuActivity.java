fd9f74b22b352ff2f882fb5d2a14d61395198d1b
TabLayoutt <101447935+ranggaezwar@users.noreply.github.com>
2023-07-04 11:04:41 +0700

    g

--- app/src/main/java/com/ranggaramadhanezwar/login/MenuActivity.java
+++ app/src/main/java/com/ranggaramadhanezwar/login/MenuActivity.java
@@ -21,86 +21,37 @@ import java.net.URLEncoder;
 import cz.msebera.android.httpclient.Header;
 
 public class MenuActivity extends AppCompatActivity {
-    private Button _inputMahasiswa;
-    private EditText _alamatEditText, _namaEditText, _nimEditText, _tahunMasukEditText, _tanggalLahirEditText;
-    private EditText _tempatLahirEditText;
-    private Spinner _jenisKelaminSpinner, _jpSpinner, _statusNikahSpinner;
+    private Button _tampilMahasiswaButton;
+    private Intent _tampilMahasiswaIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        initInputs();
-        initSaveButton();
+        initTampilMahasiswaButton();
     }
 
-    private void initSaveButton() {
-        _inputMahasiswa = findViewById(R.id.inputMahasiswa);
+    private void initTampilMahasiswaButton() {
+        _tampilMahasiswaButton = findViewById(R.id.inputMahasiswa);
 
-        _inputMahasiswa.setOnClickListener(new View.OnClickListener() {
+        _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                String alamat = _alamatEditText.getText().toString();
-                String jenisKelamin = _jenisKelaminSpinner.getSelectedItem().toString();
-                String jp = _jpSpinner.getSelectedItem().toString();
-                String nama = _namaEditText.getText().toString();
-                String nim = _nimEditText.getText().toString();
-                String statusNikah = _statusNikahSpinner.getSelectedItem().toString();
-                String tahunMasuk = _tahunMasukEditText.getText().toString();
-                String tanggalLahir = _tanggalLahirEditText.getText().toString();
-                String tempatLahir = _tempatLahirEditText.getText().toString();
+            public void onClick(View v)
+            {
+                _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(_tampilMahasiswaIntent);
 
-                try {
-                    alamat = URLEncoder.encode(alamat, "utf-8");
-                    jenisKelamin = URLEncoder.encode(jenisKelamin, "utf-8");
-                    nama = URLEncoder.encode(nama, "utf-8");
-                    nim = URLEncoder.encode(nim, "utf-8");
-                    statusNikah = URLEncoder.encode(statusNikah, "utf-8");
-                    tanggalLahir = URLEncoder.encode(tanggalLahir, "utf-8");
-                    tempatLahir = URLEncoder.encode(tempatLahir, "utf-8");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-
-                String url = "https://stmikpontianak.net/011100862/tambahMahasiswa.php" +
-                        "?nim=" + nim +
-                        "&nama=" + nama +
-                        "&jenisKelamin=" + jenisKelamin +
-                        "&tempatLahir=" + tempatLahir +
-                        "&tanggalLahir=" + tanggalLahir +
-                        "&alamat=" + alamat +
-                        "&jp=" + jp +
-                        "&statusPernikahan=" + statusNikah +
-                        "&tahunMasuk=" + tahunMasuk;
-
-                Log.d("*tw*", url);
-
-                AsyncHttpClient ahc = new AsyncHttpClient();
-
-                ahc.get(url, new AsyncHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        new AlertDialog.Builder(MenuActivity.this)
-                                .setTitle("Berhasil")
-                                .setMessage("Record berhasil disimpan")
-                                .show();
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
 
             }
-        });
-    }
-
-    private void initInputs() {
 
     }
-}
+
+
 
 
 

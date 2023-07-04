455ab6506072db44b1d9032cd8345618529dec92
TabLayoutt <101447935+ranggaezwar@users.noreply.github.com>
2023-07-05 03:54:47 +0700

    g

--- app/src/main/java/com/ranggaramadhanezwar/login/MenuActivity.java
+++ app/src/main/java/com/ranggaramadhanezwar/login/MenuActivity.java
@@ -21,8 +21,8 @@ import java.net.URLEncoder;
 import cz.msebera.android.httpclient.Header;
 
 public class MenuActivity extends AppCompatActivity {
-    private Button _tampilMahasiswaButton;
-    private Intent _tampilMahasiswaIntent;
+    private Button _tampilMahasiswaButton, _tampilForexButton;
+    private Intent _tampilMahasiswaIntent, _tampilForexIntent;
 
 
     @Override
@@ -31,6 +31,22 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         initTampilMahasiswaButton();
+        initTampilForexButton();
+    }
+
+    private void initTampilForexButton()
+    {
+        _tampilForexButton = (Button) findViewById(R.id.tampilForexButton);
+
+        _tampilForexButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                _tampilForexIntent = new Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexIntent);
+
+            }
+        });
     }
 
     private void initTampilMahasiswaButton() {
